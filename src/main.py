25903eca82060a3482f2d1e97d002628f066b8a5
Yasushi SHINOHARA <yasushi.shinohara@gmail.com>
2020-06-08 05:32:39 +0900

    The first commit to put them on stage.

---
+++ src/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding: UTF-8
+# This is created 2020/06/08 by Y. Shinohara
+# This is lastly modified 2020/06/08 by Y. Shinohara #This part is highly doubtable because of my lazyness
+import time
+ts = time.time()
+from modules.print_funcs import print_header, print_footer, print_midtime, print_endtime
+print_header()
+import sys
+import numpy as np
+import math
+import ctypes as ct
+from modules.constants import *
+
+
+tt = time.time()
+print_midtime(ts,tt)
+
+te = time.time()
+#print_endtime(ts,tt,te,param.Nt)
+
+
+print_footer() 
+sys.exit()
+
